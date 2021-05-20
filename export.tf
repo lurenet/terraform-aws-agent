795033acd94461a714cccc54a8ea849c18a89782
nickmas <nikolay.maslianikov@gmail.com>
2021-05-20 23:40:41 +0000

    add export boxfuse ip

--- export.tf
+++ export.tf
@@ -9,3 +9,10 @@ resource "local_file" "hosts" {
   content = "[build]\n${aws_instance.build.public_ip} ${var.export["host_options"]}\n\n[prod]\n${aws_instance.prod.public_ip} ${var.export["host_options"]}\n"
   file_permission = "0644"
 }
+
+resource "local_file" "prod" {
+  filename = "/root/export/prod"
+  content = "${aws_instance.prod.public_ip}"
+  file_permission = "0644"
+}
+
