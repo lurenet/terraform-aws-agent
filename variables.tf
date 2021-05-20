46feeb31f6886ed39534986f58f9b60f70b25416
nickmas <nikolay.maslianikov@gmail.com>
2021-05-20 14:57:37 +0000

    added host_options

--- variables.tf
+++ variables.tf
@@ -1,13 +1,3 @@
-#region: eu-central-1
-#instance_type: t2.small
-#ami: ami-0767046d1677be5a0  # Ubuntu 20.04 LTS
-#subnet_id: subnet-0e325064 # zone-a
-#project_name: dve13
-#app_code_user: "ubuntu" # remote user
-#hoststring: "ansible_ssh_user=ubuntu ansible_ssh_private_key_file=aws-private.pem ansible_ssh_extra_args='-o StrictHostKeyChecking=no'"
-#hostpath: "hosts"
-
-
 variable "global" {
   type    = map
   default = {
@@ -26,13 +16,9 @@ variable "ec2" {
   }
 }
 
-
-#variable "artifact" {
-#  type    = map
-#  default = {
-#    "name"    = "boxfuse"
-#    "version" = "latest"
-#    "project_url"  = "https://github.com/lurenet/dve-14-terraform_vs_gcp.git"
-#    "project_name" = "dve-14-terraform_vs_gcp"
-#  }
-#}
+variable "export" {
+  type = map
+  default =  {
+    "host_options" = "ansible_ssh_user=ubuntu ansible_ssh_private_key_file=/root/export/aws-private.pem ansible_ssh_extra_args='-o StrictHostKeyChecking=no'"
+  }
+}
